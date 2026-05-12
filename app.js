2d6c3e1bbc7864e0d08d220f7c925d3280184b1a
Sajib <iamsajib4@gmail.com>
2026-05-12 21:12:06 +0600

    3rd commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate");
  
 
 //mongoose setup
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
    .then(() => {
