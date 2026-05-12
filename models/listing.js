6469484eaec27e8b80bbdf45c6043074f061c8c2
Sajib <iamsajib4@gmail.com>
2026-05-12 20:53:04 +0600

    first commit

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
+    },
+    url: {
+      type: String,
+      default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157...",
+      set: (v) => v === "" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157..." : v,
+    }
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
+
